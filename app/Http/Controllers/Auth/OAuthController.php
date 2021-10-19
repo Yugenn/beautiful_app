bfdaece6b0c134c7d1b8b58cdec1a01c095e9f14
Yugenn <yuruseajyan@gmail.com>
2021-10-19 12:52:33 +0900

    SNS認証機能ページコピー完了

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use App\Models\IdentityProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
 
 class OAuthController extends Controller
 {
@@ -15,6 +20,40 @@ class OAuthController extends Controller
 
     public function oauthCallback()
     {
-        dd(Socialite::with('github')->user());
+        // 認証情報が返ってこなかった場合はログイン画面にリダイレクト
+        try {
+            $socialUser = Socialite::with('github')->user();
+        } catch(\Exception $e) {
+            return redirect('/login')->withErrors(['oauth_error' => '予期せぬエラーが発生しました']);
+        }
+        
+        // emailで検索してユーザーが見つかればそのユーザーを、見つからなければ新しいインスタンスを生成
+        $user = User::firstOrNew(['email' => $socialUser->getEmail()]);
+
+        // ユーザーが認証済みか確認
+        if (!$user->exists) {
+            $user->name = $socialUser->getNickname() ?? $socialUser->name;
+            $identityProvider = new IdentityProvider([
+                'id' => $socialUser->getId(),
+                'name' => 'github'
+            ]);
+
+            DB::beginTransaction();
+            try {
+                $user->save();
+                $user->identityProvider()->save($identityProvider);
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return redirect()
+                    ->route('login')
+                    ->withErrors(['transaction_error' => '保存に失敗しました']);
+            }
+        }
+
+        // ログイン
+        Auth::login($user);
+
+        return redirect()->route(RouteServiceProvider::HOME);
     }
 }
